371c7d7e09848b570fadda23b36d54492c2bf568
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-09 11:33:37 +0200

    there was a mistake in FastAPI.

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
-    title: "Choose Your Own Adventure Game API",
+    title= "Choose Your Own Adventure Game API",
     description= "api to generate cool stories",
     version="0.1.0",
     docs_url="/docs",
