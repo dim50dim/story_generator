fefd41e367b511f7fc365e0b257c21c6d1807d1a
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-09 14:29:51 +0200

    created class Story in story.py file.

--- backend/models/story.py
+++ backend/models/story.py
@@ -2,4 +2,11 @@
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, JSON
 
 from sqlalchemy.sql import func
-from sqlalchemy.orm import
+from sqlalchemy.orm import relationship
+
+from db.database import Base
+
+class Story(Base):
+  __tablename__ = "stories"
+
+  id = Column(Integer, primary_key=True,index=True)
