01b375311b50f9e14d7efaf09ce0d09543848269
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-10 11:53:24 +0200

    inside schemas created one more __init__ file.

--- backend/models/job.py
+++ backend/models/job.py
@@ -5,6 +5,7 @@ from db.database import Base
 
 class StoryJob(Base):
     __tablename__ = "story_jobs"
+    
     id = Column(Integer, primary_key=True,index=True)
     job_id = Column(String, index=True,unique=True)
     session_id = Column(String,index=True)
