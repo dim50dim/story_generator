375ff166c32467f022fbada86f5df60dafec793b
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-10 11:57:40 +0200

    inside schemas and story.py created two classes with the same argument BaseModel

--- backend/schemas/story.py
+++ backend/schemas/story.py
@@ -3,3 +3,12 @@ from datetime import datetime
 from pydantic import BaseModel
 
 
+class StoryOptionsSchemas(BaseModel):
+    text:str
+    node_id:Optional[int] = None
+
+class StoryNodeBase(BaseModel):
+    content: str
+    is_ending: bool= False
+    is_winning_ending: bool=False
+    
