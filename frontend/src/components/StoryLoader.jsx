b673d5776730afb1493a181a94a10bca08ef0d0b
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-11 09:08:56 +0200

    in StoryLoader component made at least four dependencies. created function loadStory - which will be the main point.

---
+++ frontend/src/components/StoryLoader.jsx
@@ -0,0 +1,37 @@
+import React, { useState,useEffect,useParams,useNavigate } from 'react'
+import axios from axios;
+
+const API_BASE_URL = "/api"
+const StoryLoader = () => {
+    const [story,setStory] = useState(null)
+    const{id} = useParams();
+    const navigate = useNavigate();
+    const [loading,setLoading] = useState(true)
+    const [error,setError] = useState(false);
+
+    const loadStory = async (storyId) => {
+        setLoading(true)
+        setError(null)
+
+        try{
+            const response = await axios.get(`${API_BASE_URL}/stories/${storyId}/complete`)
+            setStory(response.data)
+            setLoading(false)
+        }catch(err){
+             
+        }
+
+    }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+import axios from axios;
+
+const API_BASE_URL = "/api"export default StoryLoader
+
+const [] = useState()
+
