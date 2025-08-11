c843c0e19026bad45662c7f2804ce1e435d2f780
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-11 12:08:57 +0200

    added two useEffect()

--- frontend/src/components/StoryGame.jsx
+++ frontend/src/components/StoryGame.jsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const StoryGame = () => {
+const StoryGame = ({story,onNewStory}) => {
+  const [currentNodeId, setCurrentNodeId] = useState(null);
+    const [currentNode, setCurrentNode] = useState(null)
+    const [options, setOptions] = useState([])
+    const [isEnding, setIsEnding] = useState(false)
+    const [isWinningEnding, setIsWinningEnding] = useState(false)
+    
+    useEffect(() => {
+           if(story && story.root_node){
+            const rootNodeId = story.root_node.id 
+            setCurrentNodeId(rootNodeId)
+           }
+    },[story])
+ 
+   useEffect(() => {
+        if (currentNodeId && story && story.all_nodes) {
+            const node = story.all_nodes[currentNodeId]
+
+            setCurrentNode(node)
+            setIsEnding(node.is_ending)
+            setIsWinningEnding(node.is_winning_endig)
+
+            if (!node.is_ending && node.options && node.options.length > 0) {
+                setOptions(node.options)
+            } else {
+                setOptions([])
+            }
+        }
+    }, [currentNodeId, story])
+
   return (
     <div>
       
